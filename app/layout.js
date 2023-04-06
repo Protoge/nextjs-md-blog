ed186122c29d3380ebb6e9d119f2ec91c874fc04
RJ <rjgilo2799@gmail.com>
2023-04-06 00:25:49 -0400

    react-ga added

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,10 @@
+"use client";
 import Link from "next/link";
 import "./globals.css";
-import GoogleAnalytics from "./components/GoogleAnalytics";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import ReactGA from "react-ga";
+// import GoogleAnalytics from "./components/GoogleAnalytics";
 
 export const metadata = {
   title: "Create Next App",
@@ -20,16 +24,23 @@ export default function RootLayout({ children }) {
   );
 
   const footer = (
-    <footer className="">
+    <footer>
       <div className="border-t border-slate-500 mt-6 py-6 text-center text-slate-400">
         <h3>Developed by RJ</h3>
       </div>
     </footer>
   );
 
+  const router = useRouter();
+
+  useEffect(() => {
+    ReactGA.initialize("G-VMKE59PWJR");
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, [router.pathname]);
+
   return (
     <html lang="en">
-      <GoogleAnalytics GA_MEASUREMENT_ID="G-VMKE59PWJR" />
+      {/* <GoogleAnalytics GA_MEASUREMENT_ID="G-VMKE59PWJR" /> */}
       <body>
         <div className="mx-auto max-w-2xl px-6">
           {header}
